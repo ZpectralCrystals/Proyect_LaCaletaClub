6dd1482109891bb3e0f90abd47d5376f2af89de8
Gustavo Galvez Delgado <59744063+ZpectralCrystals@users.noreply.github.com>
2025-05-07 00:50:07 -0500

    pagina en borrador

---
+++ src/components/Carta.tsx
@@ -0,0 +1,16 @@
+const Carta = () => {
+    return (
+      <section id="carta" className="section">
+        <h2>Nuestra Carta</h2>
+        <ul>
+          <li>Ceviche Clásico</li>
+          <li>Leche de Tigre</li>
+          <li>Arroz con Mariscos</li>
+          <li>Jalea Mixta</li>
+        </ul>
+      </section>
+    );
+  };
+  
+  export default Carta;
+  
