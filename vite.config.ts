f8d808c7b31758fb0fae9b1b7b6a7be57562f7a9
timothy-coder <nicopar666xd@gmail.com>
2025-05-19 21:29:24 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // <--- Esto es importante
+      '@': path.resolve(__dirname, 'src'),  // <-- Aquí defines '@' como la carpeta src
     },
   },
-});
+})
