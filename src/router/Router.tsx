15b31875237309514650bca6a5e5405b945b19ae
Gustavo Galvez Delgado <59744063+ZpectralCrystals@users.noreply.github.com>
2025-05-20 20:34:42 -0500

    update

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -9,16 +9,11 @@ import Recomendaciones from "../pages/Recomendaciones/Recomendaciones";
 import Carrito from "../pages/Carrito/Carrito";
 import Paginas from "../pages/Paginas/Paginas";
 
-<<<<<<< Updated upstream
 // Páginas administrativas
-=======
-// Páginas administrativas (privadas)
->>>>>>> Stashed changes
 import Category from "../pagesAdmin/Category/Category";
 import Discounts from "../pagesAdmin/Discounts/Discounts";
 import Products from "../pagesAdmin/Products/Products";
 
-<<<<<<< Updated upstream
 // Layout general
 import Layout from "@/components/Layout/Layout";
 
@@ -47,37 +42,3 @@ export function Router() {
 }
 
 export default Router;
-=======
-// Layouts
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/Footer";
-
-
-export function Router() {
-    return (
-        <BrowserRouter>
-            <div className="pt-24 bg-white min-h-screen">
-                <Navbar />
-
-                <Routes>
-                    {/* Rutas públicas */}
-                    <Route path="/" element={<Inicio />} />
-                    <Route path="/menu" element={<Menu />} />
-                    <Route path="/blog" element={<Blog />} />
-                    <Route path="/contacto" element={<Contacto />} />
-                    <Route path="/recomendaciones" element={<Recomendaciones />} />
-                    <Route path="/carrito" element={<Carrito />} />
-                    <Route path="/paginas" element={<Paginas />} />
-                    <Route path="/admin/categorias" element={<Category />} />
-                    <Route path="/admin/productos" element={<Products />} />
-                    <Route path="/admin/promociones" element={<Discounts />} />
-                </Routes>
-
-                <Footer />
-            </div>
-        </BrowserRouter>
-    );
-};
-
-export default Router;
->>>>>>> Stashed changes
