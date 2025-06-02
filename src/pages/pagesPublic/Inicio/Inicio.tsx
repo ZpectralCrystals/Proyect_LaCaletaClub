19ecdc45fb4cacb91eae0293a3e460c414eda83a
Gustavo Galvez Delgado <59744063+ZpectralCrystals@users.noreply.github.com>
2025-06-01 22:11:31 -0500

    feat: add swiper

--- src/pages/pagesPublic/Inicio/Inicio.tsx
+++ src/pages/pagesPublic/Inicio/Inicio.tsx
@@ -1,12 +1,13 @@
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { supabase } from "@/services/supabase";
 import HeroSlider from "@/components/HeroSlider/HeroSlider";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+
 
 interface Producto {
   id: number;
