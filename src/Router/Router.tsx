7af6acefac31d54404a48ad0b136a037322f97f5
timothy-coder <nicopar666xd@gmail.com>
2025-06-05 23:02:36 -0500

    commit

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -16,7 +16,7 @@ import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
 
 // Login y registro
 import AuthRoute from "@/components/ProtectedRoute/AuthRoute";
-import ImagesCharge from "@/pages/pagesAdmin/ImagesCharge/ImagesCharge";
+import Pointsuser from "@/pages/pagesAdmin/Pointsuser/Pointsuser";
 import CajaAdmin from "@/pages/pagesAdmin/Caja/Caja";
 import Pedido from "@/pages/pagesAdmin/Pedido/Pedido";
 import PedidoView from "@/pages/pagesAdmin/PedidoView/PedidoView";
@@ -65,7 +65,7 @@ export function Router() {
           <Route path="/admin/comentarios" element={<CommentsAdmin />} />
           <Route path="/admin/recomendaciones" element={<RecomendacionesAdmin />} />
           <Route path="/admin/reportes" element={<ReportesAdmin />} />
-          <Route path="/admin/carga-image" element={<ImagesCharge />} />
+          <Route path="/admin/puntos" element={<Pointsuser />} />
           <Route path="/admin/cobranza" element={<CajaAdmin />} />
           <Route path="/admin/pedidos" element={<Pedido />} />
           <Route path="/admin/ver-pedido" element={<PedidoView />} />
--- src/pages/pagesAdmin/AppSidebar/AppSidebar.tsx
+++ src/pages/pagesAdmin/AppSidebar/AppSidebar.tsx
@@ -128,9 +128,9 @@ function SidebarContentBody({ onItemClick }: SidebarContentBodyProps) {
 
             {[2].includes(user.role) && (
               <SidebarMenuButton asChild>
-                <Link to="/admin/carga-image" onClick={onItemClick} className="flex items-center gap-3 px-3 py-2 rounded-md hover:bg-muted transition-colors">
+                <Link to="/admin/puntos" onClick={onItemClick} className="flex items-center gap-3 px-3 py-2 rounded-md hover:bg-muted transition-colors">
                   <HardDriveUpload className="w-4 h-4" />
-                  <span>Carga de Imágenes</span>
+                  <span>Puntos</span>
                 </Link>
               </SidebarMenuButton>
             )}
